0732245de9437bc692700197e99912361584dd63
tai-kun <taikun.dev@gmail.com>
2023-09-05 09:44:32 +0900

    chore: Improve test logs

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,29 +5,27 @@ NODE_VERSIONS='16 18 20'
 GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m'
-CLEAR='\033[2K\r'
 
 if [ -n "$CI" ]; then
     GREEN=''
     RED=''
     NC=''
-    CLEAR=''
 fi
 
 function run() {
     local rc
     local LOG_FILE
 
-    echo -n "running: $*"
+    echo -n "running: $* ... "
 
     LOG_FILE="$(mktemp)"
     bash -c "$* >> $LOG_FILE 2> $LOG_FILE"
     rc=$?
 
     if [ $rc -eq 0 ]; then
-        echo -e "$CLEAR${GREEN}success: $* $NC"
+        echo -e "${GREEN}success${NC}"
     else
-        echo -e "$CLEAR${RED}failed: $* $NC"
+        echo -e "${RED}failed${NC}"
         echo
         cat "$LOG_FILE"
         echo
@@ -38,15 +36,15 @@ function run() {
     return $rc
 }
 
-function run_test() {
+function run_tests() {
     local rs
     local mod=$1
     local node_version=$2
 
     if [ -z "$node_version" ]; then
-        echo -e "${CLEAR}[$mod] Running tests with default node ${GREEN}$(node --version)${NC}"
+        echo -e "[$mod] Running tests with default node ${GREEN}$(node --version)${NC}"
     else
-        echo -e "${CLEAR}[$mod] Running tests with node version ${GREEN}${node_version}${NC}"
+        echo -e "[$mod] Running tests with node version ${GREEN}${node_version}${NC}"
 
         cp package.json package.json.bak
         volta pin "node@$node_version"
@@ -73,13 +71,13 @@ rs=0
 if type volta >/dev/null 2>&1; then
     for mod in cjs esm; do
         for node_version in $NODE_VERSIONS; do
-            (cd "examples/$mod" && run_test "$mod" "$node_version") || rs=1
+            (cd "examples/$mod" && run_tests "$mod" "$node_version") || rs=1
             echo
         done
     done
 else
     for mod in cjs esm; do
-        (cd "examples/$mod" && run_test "$mod") || rs=1
+        (cd "examples/$mod" && run_tests "$mod") || rs=1
         echo
     done
 fi
