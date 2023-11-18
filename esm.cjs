60acfdd4ead33a89bd91e6e23893a37d12cc0e7e
tai-kun <taikun.dev@gmail.com>
2023-11-18 16:04:22 +0900

    chore: Remove test runner file extension handling https://github.com/swc-project/swc-node/commit/8dbea4fd0ac154cbc72088202a8ddf707e40fa13#diff-39fad42aba9db636f50b7209d5b35a6d43d4ba60823a0ab3e9d840ef4c09ee6dL62

--- esm.cjs
+++ esm.cjs
@@ -6,7 +6,7 @@ if (fileIndex in process.argv) {
   const { pathToFileURL } = require("node:url");
 
   const fileAbsPath = resolve(process.argv[fileIndex]);
-  const testRunnerFile = `${pathToFileURL(fileAbsPath)}${transpiledFileExtension()}`;
+  const testRunnerFile = pathToFileURL(fileAbsPath);
 
   process.env.NODE_ENV = "test";
   process.env.TEST_RUNNER_FILE = testRunnerFile;
@@ -15,23 +15,4 @@ if (fileIndex in process.argv) {
   const assert = require("node:assert/strict");
 
   global.testRunner = Object.assign(test, { assert });
-}
-else {
-  // ignore (In Node.js v20 this script is called twice, once for the main file and once for the test file.)
-}
-
-function transpiledFileExtension() {
-  if (
-    // --loader @swc-node/register/esm
-    // --loader=@swc-node/register/esm
-    // --experimental-loader @swc-node/register/esm
-    // --experimental-loader=@swc-node/register/esm
-    /,--(experimental-)?loader[,=]@swc-node\/register\/esm,/.test(`,${process.execArgv},`)
-  ) {
-    // Why ".mjs"?:
-    // https://github.com/swc-project/swc-node/blob/9f674cd67091192b1fe62befd13cf4b61a6377e5/packages/register/esm.mts#L73
-    return ".mjs";
-  }
-
-  return "";
 }
