4ee0ce22cbc9d7df477edec069891391c331240c
tai-kun <taikun.dev@gmail.com>
2023-11-24 11:14:47 +0900

    chore: Update dependencies in examples

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-(cd examples/cjs && npx npm-check-updates --upgrade --reject @types/node)
-(cd examples/esm && npx npm-check-updates --upgrade --reject @types/node)
-(cd examples/v20_6_0 && npx npm-check-updates --upgrade --reject @types/node)
+(cd examples/cjs && npx npm-check-updates --upgrade --reject @types/node && npm i)
+(cd examples/esm && npx npm-check-updates --upgrade --reject @types/node && npm i)
+(cd examples/v20_6_0 && npx npm-check-updates --upgrade --reject @types/node && npm i)
