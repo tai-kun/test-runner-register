48d6819f29e91d4e648333d4ef1ffaa8081c3271
tai-kun <taikun.dev@gmail.com>
2023-11-24 12:05:14 +0900

    chore: Inprove error message

--- scripts/test-config.sh
+++ scripts/test-config.sh
@@ -6,8 +6,10 @@ files=$(jq -r '.files[]' package.json)
 exports=$(jq -r '.exports | to_entries | .[] | .value | to_entries | .[] | .value' package.json)
 
 for file in $exports; do
-    if ! echo "$files" | grep -q "${file#./}"; then
-        echo "Error: $file is not listed in the files field of package.json"
+    file="${file#./}"
+
+    if ! echo "$files" | grep -q "$file"; then
+        echo "Error: \"$file\" is not listed in the files field of package.json"
         rs=1
     fi
 done
