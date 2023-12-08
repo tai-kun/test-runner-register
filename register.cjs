5d3bf296a8db3edfe883cc7accffc12847e1ddd2
tai-kun <taikun.dev@gmail.com>
2023-12-08 19:25:19 +0900

    feat: Add register function

---
+++ register.cjs
@@ -0,0 +1,43 @@
+const { resolve } = require("node:path");
+
+const ARGV = ["/path/to/node", "/path/to/file"];
+const fileIndex = ARGV.indexOf("/path/to/file");
+
+/**
+ * @param {object} [setup] 
+ * @param {string[]} [setup.argv]
+ * @param {string[]} [setup.execArgv]
+ * @param {(abs: string) => string} [setup.getFilePath]
+ */
+exports.register = function register(setup) {
+  setup = setup || {};
+  setup.argv = setup.argv || process.argv;
+  setup.execArgv = setup.execArgv || process.execArgv;
+  setup.getFilePath = setup.getFilePath || (x => x);
+
+  if (!(fileIndex in setup.argv)) {
+    throw new Error("The `argv` array must contain the file path.");
+  }
+
+  if (/,--test[,=]/.test(`,${setup.execArgv},`)) {
+    process.env.NODE_ENV = "test";
+    process.env.TEST_RUNNER_FILE =
+      setup.getFilePath(resolve(setup.argv[fileIndex]));
+
+    if (/,--watch,/.test(`,${setup.execArgv},`)) {
+      process.env.TEST_RUNNER_WATCH = "true";
+    }
+  } else {
+    try {
+      const test = require("node:test");
+      const assert = require("node:assert/strict");
+
+      global.testRunner = Object.assign(test, { assert });
+    } catch (_) {
+      console.warn(
+        "The `node:test` module is not supported in this environment." +
+        "So, the `testRunner` global variable is not available."
+      );
+    }
+  }
+};
